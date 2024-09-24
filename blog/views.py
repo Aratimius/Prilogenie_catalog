b7d3237dc305068c8461a466ce280dff0fa9f285
Aratimius <aratimius@gmail.com>
2024-09-24 05:40:00 +0300

    CBV (НЕ ГРУЗЯТСЯ КАРТИНКИ В ПРИЛОЖЕНИИ blog, СЧЕТЧИК ПРОСМОТРОВ УВЕЛИЧИВАЕТСЯ НА 2)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from pytils.translit import slugify
 
 from blog.models import Blog
@@ -10,6 +10,7 @@ class BlogListView(ListView):
     model = Blog
 
     def get_queryset(self):
+        """Фильтрация по опубликованным статьсям"""
         queryset = super().get_queryset()
         queryset = queryset.filter(publication_sign=True)
         return queryset
@@ -21,6 +22,7 @@ class BlogCreateView(CreateView):
     success_url = reverse_lazy('blog:list')
 
     def form_valid(self, form):
+        """Автоматическое создание slug"""
         if form.is_valid():
             new_blog = form.save()
             new_blog.slug = slugify(new_blog.title)
@@ -31,25 +33,29 @@ class BlogCreateView(CreateView):
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'content', 'preview', 'publication_sign')
-    success_url = reverse_lazy('blog:list')
+
+    def get_success_url(self):
+        """Перенаправление после редактирования на просмотр блога"""
+        return reverse("blog:detail", args=[self.kwargs.get("pk")])
 
 
 class BlogDetailView(DetailView):
     model = Blog
 
     def get_context_data(self, **kwargs):
+        """Зпередать объекту заголовок статьи"""
         context_data = super().get_context_data(**kwargs)
         object = Blog.objects.get(pk=self.kwargs['pk'])
         context_data['title'] = object.title
         return context_data
 
+    #  МЕТОД РАБОТАЕТ КРИВО, ДОБАВЛЯЯ ПО 2 ПРОСМОТРА ПРИ ОБНОВЛЕНИИ СТРАНИЦЫ. ПОЧЕМУ?
     def get_object(self, queryset=None):
+        """Метод для увеличения счетчика просмотров"""
         self.object = super().get_object(queryset)
         self.object.views += 1
         self.object.save()
         return self.object
-
-
 
 
 class BlogDeleteView(DeleteView):
